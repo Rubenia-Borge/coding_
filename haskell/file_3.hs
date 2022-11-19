667de5692cd877bec39a1671ea90e5d9034221ff
Rubenia Borge <rubeniaolaska@gmail.com>
2022-11-18 22:53:48 -0600

    Update file_3.hs

--- haskell/file_3.hs
+++ haskell/file_3.hs
@@ -1,1 +1,22 @@
 
+
+main = do 
+   let var1 = 50 
+   let var2 = 89 
+   putStrLn "Sum var1 + var2:" 
+   print(var1 + var2)
+   
+   let var3 = 800
+   let var4 = 900
+   putStrLn "Sum var3 + var4:"
+   print(var3 + var4)
+   
+   let var5 = 39
+   let var6 = 77
+   putStrLn "Sum var5 + var6"
+   print(var5 + var6)
+   
+   let var7 = 54
+   let var8 = 27
+   putStrLn "Sum var7 + var8"
+   print(var7 + var8)
